e03e3f2b3ca5ba823dc8f0b1bdcdd25dc5480f6e
Andrii Grytsenko <ahrytsenko@gmail.com>
2020-01-10 14:43:48 +0200

    Initial commit

---
+++ NBU_ExchangeRates/src/nbu_exchangerates/NBU_ExchangeRates.java
@@ -0,0 +1,9 @@
+package nbu_exchangerates;
+
+public class NBU_ExchangeRates {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ NBU_ExchangeRates/src/nbu_exchangerates/dataModel/Model.java
@@ -0,0 +1,16 @@
+package nbu_exchangerates.dataModel;
+
+public class Model {
+    
+    public String getCurrencyRate() {
+        return "getCurrencyRate()";
+    }
+    
+    public String getCurrencyRate(String date) {
+        return "getCurrencyRate(String date)";
+    }
+    
+    public String getCurrencyRate(String CurrencyID, String date) {
+        return "getCurrencyRate(String CurrencyID, String date)";
+    }
+}
