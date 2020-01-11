23877205de7594235c7dfe71867c908bb88b4eb0
Andrii Grytsenko <ahrytsenko@gmail.com>
2020-01-11 09:45:10 +0200

    json.txt added

---
+++ NBU_ExchangeRates/src/nbu_exchangerates/dataModel/CurrencyRate.java
@@ -0,0 +1,15 @@
+package nbu_exchangerates.dataModel;
+
+import java.util.*;
+
+public class CurrencyRate {
+    
+    private HashMap<String, String> fieldMap;
+    private int currencyID;
+    private String currencyCode;
+    private String currencyName;
+    private double rate;
+    private Date exchangeDate;
+    
+    
+}
